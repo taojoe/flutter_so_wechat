6ae112809b7643d38cc71c5a634f2bc175d02a4e
joe <taojoe@gmail.com>
2020-03-12 17:36:16 +0800

    增加stream

--- ios/Classes/SoWechatPlugin.m
+++ ios/Classes/SoWechatPlugin.m
@@ -1,11 +1,16 @@
 #import "SoWechatPlugin.h"
 
+@interface SoWechatPlugin() <FlutterStreamHandler>
+@property (copy, nonatomic)   FlutterEventSink   flutterEventSink;
+@end
+
 @implementation SoWechatPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     FlutterMethodChannel* channel = [FlutterMethodChannel methodChannelWithName:@"com.github.taojoe.so_wechat/method" binaryMessenger:[registrar messenger]];
     FlutterEventChannel* stream = [FlutterEventChannel eventChannelWithName:@"com.github.taojoe.so_wechat/event" binaryMessenger:registrar.messenger];
     SoWechatPlugin* instance = [[SoWechatPlugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
+    [stream setStreamHandler:instance];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
@@ -17,6 +22,15 @@
   }else {
       result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   }
+}
+
+-(FlutterError*)onListenWithArguments:(id)arguments eventSink:(FlutterEventSink)events {
+    self.flutterEventSink = events;
+    return nil;
+}
+
+-(FlutterError*)onCancelWithArguments:(id)arguments {
+    return nil;
 }
 
 @end
